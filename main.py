28c1b45e7f2e77be12dc859d250481b6e3f4072e
socialhi2 <150467326+socialhi2@users.noreply.github.com>
2026-02-18 00:49:11 -0500

    Bug Fixes + Utility Message
    Added:
    Utility Messages:
         1. The end message was reached
         2. The loss message reached
         3. Reached if total < 21
         4. Reached if user input is 1 (while game is running)
         5. Reached if user input is 2 (while game is running)
         6. Reached if user total > 21
         7. The win message is reached
    Comments to help users understand parts of code
    Bool to check if user is in actively playing
    Auto lose if player's hand is >21
    
    Bugs
    11-13 still shows 11-13 instead of face cards
    
    Bug Fixes:
    Game will infinitely repeat itself if total > 21
    
    Notes: I am tired and this really did not take long to fix. U GOT IT, I GOT IT, WE ARE GOATS!!!
    A GOAT OF MUSIC LUKE CHIANG
    https://open.spotify.com/track/6GBBnbNFzAqwowHOjHQysr?si=c6067210871845c2

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import deck
 from deck import calculate_total
 
-played = [0]
 
-# Starts Simulation
+# Starts Simulation (THIS IS NOT UPDATED TO RECENT CODE)
+#played = [0]
 #for z in range(1, 50):
 #    # rolls
 #    x = deck.cardRoll()
@@ -13,22 +13,31 @@ played = [0]
 #    if deck.cardCheck(played, x):
 #        while deck.cardCheck(played, x):
 #             x = deck.cardCheck(played, deck.cardRoll())
-
 #x1, y1 = deck.cardRoll()
 #x2, y2 = deck.cardRoll()
 #print("Your cards: ", x1, y1, " and ", x2, y2, "Your total is: ", x1 + x2)
 
-def end_message(wn):
-    if wn == 1:
+
+
+
+
+def end_message(total):
+    # print("Utility 1: End message reached")
+    if total < 21:
+        # print("Utility 7: Win message reached")
         print("You have won!")
-    if wn == 0:
+    if total > 21:
+        # print("Utility 2: Loss message reached")
         print("You lost!")
 
 def play_blackjack():
     cc = 0
     game_close = False
+
     # Main Menu Loop
     while not game_close:
+        # Sets player to not be in game when booting program
+        in_game = False
         usr = (int(input("Enter 1 to play 2 to quit \n")))
         # User entering 2 for quit
         if usr == 2:
@@ -36,6 +45,7 @@ def play_blackjack():
             continue
         # Main Game Loop
         if usr == 1:
+            in_game = True
             played_cards = []
             for _ in range(2):
                 card = deck.cardRoll()
@@ -47,16 +57,30 @@ def play_blackjack():
             print(f"Total: ", total)
 
             #Goes here if player does not start with 21
-            if total < 21:
-                usr = int(input("Enter 1 to hit | Enter 2 to stay \n"))
-                if usr == 1:
-                    card = deck.cardRoll()
-                    while deck.cardCheck(played_cards, card):
+            while in_game:
+                if total <= 21:
+                    # print("Utility 3: Reached if total < 21")
+                    usr = int(input("Enter 1 to hit | Enter 2 to stay \n"))
+
+                    if usr == 1:
+                        # print("Utility 4: Reached if usr input is 1")
                         card = deck.cardRoll()
-                    played_cards.append(card)
-                    total = calculate_total(played_cards)
-                    print(f"Card: {card[0]} of {card[1]}")
-                    print(f"Total: {total}")
+                        while deck.cardCheck(played_cards, card):
+                            card = deck.cardRoll()
+                        played_cards.append(card)
+                        total = calculate_total(played_cards)
+                        print(f"Card: {card[0]} of {card[1]}")
+                        print(f"Total: {total}")
+
+                    if usr == 2:
+                        in_game = False
+                        # print("Utility 5: Reached if usr input is 2")
+                        end_message(total)
+
+                if total > 21:
+                    in_game = False
+                    # print("Utility 6: Reached if total > 21")
+                    end_message(total)
 
 
 play_blackjack()
